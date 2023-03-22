5f6527c6a55eb3067a238d2094b1177050462586
ABHISHEK KUMAR PRADHAN <86228414+AbhiAKP@users.noreply.github.com>
2023-03-22 22:17:03 +0530

    minor changes: added some dependencies

--- backend/routes/home.js
+++ backend/routes/home.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const routes = express.Router();
 const verifyJWT = require('../middleware/verifyJWT');
+const routes = express.Router();
 
 routes.get('/', verifyJWT, (req, res) => {
     res.end("hello world");
